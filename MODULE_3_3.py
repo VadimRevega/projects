0d71b614939e0c2f0be6a836f3a28629cc1b5495
VadimRevega <vadim.revega@gmail.com>
2024-10-10 11:29:13 +0300

    Add files via upload
    Signed-off-by: VadimRevega <vadim.revega@gmail.com>

---
+++ MODULE_3_3.py
@@ -0,0 +1,21 @@
+#Функция с параметрами по умолчанию
+def print_params(a = 1, b = 'строка', c = True):
+    print(a,b,c)
+#Распаковка параметров:
+values_list = [54.32, 'точка', False]
+
+values_dict = {'a' : 1, 'b' : 'строка', 'c' : True}
+
+values_list_2 = [54.32, 'Строка']
+
+
+
+
+print_params()
+print_params(b=25)
+print_params(c = [1,2,3])
+print_params(*values_list)
+print_params(**values_dict)
+print_params(values_list_2)
+print_params(*values_list_2, 42)
+
